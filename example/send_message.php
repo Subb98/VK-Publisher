cd41ed7e6f14ca16713155d93bfaf33b284ffa04
Subb98 <subb98@yandex.ru>
2019-04-06 18:17:54 +0500

    Update example

--- example/send_message.php
+++ example/send_message.php
@@ -13,8 +13,8 @@ $settings->setAlbumId(256572563);
 $settings->setAccessToken('fe8c3650bdf7ad7185598c5a32c43a4e8d38bf1135e7d7920fdafd5dc5615949ba3fc7b461d6fd120b97f');
 $settings->setApiVersion('5.92');
 
-$photo_uploader = new PhotoUploader($settings, new PhotoValidator);
-$post_sender = new PostSender($settings);
+$photoUploader = new PhotoUploader($settings, new PhotoValidator);
+$postSender = new PostSender($settings);
 
-$photo_uri = $photo_uploader->uploadPhotoToAlbum(__DIR__ . '/img/peter-as-superman.jpg');
-$post_sender->sendPostToWall('Message sends by "VK Publisher" PHP library', [$photo_uri]);
+$photoUri = $photoUploader->uploadPhotoToAlbum(__DIR__ . '/img/peter-as-superman.jpg');
+$postSender->sendPostToWall('Message sends by "VK Publisher" PHP library', [$photoUri]);
